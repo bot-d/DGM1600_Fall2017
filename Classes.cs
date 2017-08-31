2ed872eb159840bbf17a28971ddb81df7a9e5e48
bot-d <31229152+bot-d@users.noreply.github.com>
2017-08-30 20:39:52 -0600

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+    public class One {
+		public int moneyBag = 1000000;  
+		public float fuel = 0.01f;
+
+	 	private string name = "Bob";
+	}
+	public class Two {
+	}
+	public class Three {
+
+	}
+
+	public class Four {
+
+	}
+
+	public class Five {
+		
+	}
+
+	}
+
+	
+
